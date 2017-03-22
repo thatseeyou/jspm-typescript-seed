e6678b575b5e0c25d8acf5ab19e20974f8c106e9
Sang Young Oh <sangyoung@gmail.com>
2017-03-22 14:49:37 +0900

    clean

--- src/app/top10.component.ts
+++ src/app/top10.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit }      from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NewsItem } from './news-item';
-import { NewsService }from './news.service';
+import { NewsService } from './news.service';
 
 @Component({
   //moduleId: module.id,
